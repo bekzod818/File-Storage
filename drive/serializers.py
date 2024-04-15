abe13857a7bce69ba86b896cd888b1c3cd6f4bd8
bekzod818 <bekzod030900@gmail.com>
2024-04-15 12:00:00 +0500

    fix: models

--- drive/serializers.py
+++ drive/serializers.py
@@ -1,9 +1,26 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
-from .models import File
+from .models import File, Sharing
 
 
 class FileSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
         fields = "__all__"
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email"]
+
+
+class SharingSerializer(serializers.ModelSerializer):
+    shared_with = UserSerializer(read_only=True)
+    shared_by = UserSerializer(read_only=True)
+    shared_file = FileSerializer(read_only=True)
+
+    class Meta:
+        model = Sharing
+        fields = ["id", "shared_file", "shared_with", "shared_by", "shared_at"]
