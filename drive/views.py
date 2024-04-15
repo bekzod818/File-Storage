c141773a536ff0dc12ed6be63916c435fab34dec
bekzod818 <bekzod030900@gmail.com>
2024-04-15 14:00:00 +0500

    fix: user registration

--- drive/views.py
+++ drive/views.py
@@ -1,57 +1,60 @@
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import File, Sharing
+from .permissions import IsOwnerOrReadOnly
 from .serializers import FileSerializer, UserSerializer
 
 
 class FileUploadAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+
     def post(self, request, *args, **kwargs):
         serializer = FileSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(owner=request.user)  # Assign the owner as the current user
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(owner=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class FileDeleteAPIView(APIView):
+    permission_classes = (
+        IsAuthenticated,
+        IsOwnerOrReadOnly,
+    )
+
     def delete(self, request, file_id, *args, **kwargs):
-        try:
-            file_obj = File.objects.get(id=file_id)
-            # Check permissions if needed
-            if file_obj.owner == request.user:  # Check if the user owns the file
-                file_obj.delete()
-                return Response(
-                    "File deleted successfully", status=status.HTTP_204_NO_CONTENT
-                )
-            else:
-                return Response(
-                    "You do not have permission to delete this file",
-                    status=status.HTTP_403_FORBIDDEN,
-                )
-        except File.DoesNotExist:
-            return Response("File not found", status=status.HTTP_404_NOT_FOUND)
+        file_obj = get_object_or_404(File, id=file_id)
+        # Check permissions if needed
+        self.check_object_permissions(request, file_obj)
+        file_obj.delete()
+        return Response("File deleted successfully", status=status.HTTP_204_NO_CONTENT)
 
 
 class FileShareAPIView(APIView):
-    def post(self, request, file_id, format=None):
-        try:
-            file_obj = File.objects.get(id=file_id)
-            shared_with_users = request.data.getlist(
-                "shared_with_users"
-            )  # Assuming you receive a list of user IDs to share with
-            for user_id in shared_with_users:
-                shared_user = User.objects.get(id=user_id)
+    permission_classes = (
+        IsAuthenticated,
+        IsOwnerOrReadOnly,
+    )
+
+    def post(self, request, file_id, *args, **kwargs):
+        file_obj = get_object_or_404(File, id=file_id)
+        self.check_object_permissions(request, file_obj)
+        shared_with_users = request.data.getlist(
+            "shared_with_users"
+        )  # Assuming you receive a list of user IDs to share with
+        for user_id in shared_with_users:
+            shared_user = User.objects.filter(id=user_id).first()
+            if shared_user:
                 Sharing.objects.create(
                     file=file_obj, shared_with=shared_user, shared_by=request.user
                 )
-            return Response("File shared successfully", status=status.HTTP_201_CREATED)
-        except File.DoesNotExist:
-            return Response("File not found", status=status.HTTP_404_NOT_FOUND)
+        return Response("File shared successfully", status=status.HTTP_201_CREATED)
 
 
 class UserRegistrationAPIView(generics.CreateAPIView):
