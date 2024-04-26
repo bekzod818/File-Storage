4b36936d2e1db28d86c58a49fc3756a8315b9d09
bekzod818 <bekzod030900@gmail.com>
2024-04-26 23:51:37 +0500

    fix: reqs updated

--- config/settings.py
+++ config/settings.py
@@ -95,10 +95,21 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "<db_name>",
+        "USER": "<db_username>",
+        "PASSWORD": "<password>",
+        "HOST": "<db_hostname_or_ip>",
+        "PORT": "<db_port>",
     }
 }
 
